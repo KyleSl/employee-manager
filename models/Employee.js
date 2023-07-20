fc4f55fd71f884ff8139982f66a41303d0bf45b4
Kyle <kyleslaughter8@gmail.com>
2023-07-19 19:29:55 -0600

    added models for sql database

---
+++ models/Employee.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Employee extends Model {}
+
+Employee.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+        type: DataTypes.VARCHAR(30),
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+    role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "role",
+            key: "id",
+        },
+    },
+    manager_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: "employee",
+            key: "id"
+        },
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "employee",
+  }
+);
+
+module.exports = Employee;
